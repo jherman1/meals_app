933898ed53cfed8b665d1afb3f600c82efeb1a81
Jake Herman <jake@Jakes-MacBook-Pro-2.local>
2024-08-28 16:57:19 -0600

    Added tab-based navigation, and refactored some of the code to properly handle tabs

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -11,7 +11,7 @@ class CategoriesScreen extends StatelessWidget {
     final filteredMeals = dummyMeals
         .where((item) => item.categories.contains(category.id))
         .toList();
-        
+
     Navigator.push(
       context,
       MaterialPageRoute(
@@ -23,27 +23,23 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick your Category'),
+    return GridView(
+      padding: const EdgeInsets.all(24),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(24),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: [
-          for (final Category category in availableCategories)
-            CategoryGridItem(
-                category: category,
-                onSelectCategory: () {
-                  _selectCategory(context, category);
-                })
-        ],
-      ),
+      children: [
+        for (final Category category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          )
+      ],
     );
   }
 }
