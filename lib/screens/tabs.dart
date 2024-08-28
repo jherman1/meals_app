eca7d9dad6c45365386786c4148736eab94e4d5e
Jake Herman <jake@Jakes-MacBook-Pro-2.local>
2024-08-28 17:13:16 -0600

    initial code for ability to 'favorite' meals.

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,23 +1,20 @@
-
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/meals.dart';
 
 class TabsScreen extends StatefulWidget {
-
   const TabsScreen({super.key});
 
   @override
   State<TabsScreen> createState() {
     return _TabsScreenState();
   }
-
-
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-
   int _selectedPageIndex = 0;
+  final List<Meal> _favoritedMeals = [];
 
   void _selectPage(int index) {
     setState(() {
@@ -25,14 +22,28 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _toggleMealFavoriteStatus(Meal meal) {
+    final isFavorited = _favoritedMeals.contains(meal);
+
+    if (isFavorited) {
+      _favoritedMeals.remove(meal);
+    } else {
+      _favoritedMeals.add(meal);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    Widget activePage = const CategoriesScreen();
+    Widget activePage = CategoriesScreen(
+      onToggleFavorite: _toggleMealFavoriteStatus,
+    );
     var activePageTitle = 'Categories';
 
-    if(_selectedPageIndex == 1) {
-      activePage = const MealsScreen(meals: []);
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(
+        meals: [],
+        onToggleFavorite: _toggleMealFavoriteStatus,
+      );
       activePageTitle = 'Your Favorites';
     }
 
@@ -46,11 +57,11 @@ class _TabsScreenState extends State<TabsScreen> {
         currentIndex: _selectedPageIndex,
         selectedItemColor: Colors.red,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.set_meal), label: 'Categories'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.set_meal), label: 'Categories'),
           BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites')
         ],
       ),
     );
   }
-
 }
