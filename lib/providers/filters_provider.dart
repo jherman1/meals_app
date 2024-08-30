1857f24d749d45c9678a650020ef5961b534c37a
Jake Herman <jake@Jakes-MacBook-Pro-2.local>
2024-08-29 22:43:08 -0600

    Implemented a filteredMealProvider (dependent provider) and refactored the code so that this provider provides the meals based on the filters.  This moved logic from the TabsScreen build() method to the provider itself.

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/providers/meals_provider.dart';
 
 enum Filter {
   glutenFree,
@@ -8,12 +9,13 @@ enum Filter {
 }
 
 class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
-  FiltersNotifier() : super({
-    Filter.glutenFree: false,
-    Filter.lactoseFree: false,
-    Filter.vegetarian: false,
-    Filter.vegan: false,
-  });
+  FiltersNotifier()
+      : super({
+          Filter.glutenFree: false,
+          Filter.lactoseFree: false,
+          Filter.vegetarian: false,
+          Filter.vegan: false,
+        });
 
   void setFilter(Filter filter, bool isActive) {
     //State must be updated in an immutable fashion.
@@ -26,8 +28,33 @@ class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
   void setFilters(Map<Filter, bool> chosenFilters) {
     state = chosenFilters;
   }
-
 }
 
+final filtersProvider =
+    StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
+        (ref) => FiltersNotifier());
 
-final filtersProvider = StateNotifierProvider<FiltersNotifier, Map<Filter,bool>>((ref) => FiltersNotifier());
+final filteredMealProvider = Provider((ref) {
+  var meals = ref.watch(mealsProvider);
+  var activeFilters = ref.watch(filtersProvider);
+  
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+
+    if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+
+    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+
+    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+
+    return true;
+  }).toList();
+});
